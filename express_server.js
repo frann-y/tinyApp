0182af16e691894a0856b54f265aa2c67a5c8008
frann-y <fgranger00.com>
2019-01-22 22:31:09 +0000

    w2d3 cookies in express complete

--- express_server.js
+++ express_server.js
@@ -1,10 +1,22 @@
+//the body-parser library will allow us to access POST request parameters, such as req.body.longURL
+//the bodyParse library will allow us to acces Post REQUEST PARAMETERS
+//such as req.body.longURL which we will store in a variable called urlDatabase
+//later we will store them in a real database. for now just communication between 
+//server ad client
+const bodyParser = require("body-parser");
+
 const uuidv4 = require('uuid/v4');
 //require should be grouped together
 var express = require("express");
 var app = express();
-var PORT = 8080; // default port 8080
+var PORT = 3001; // default port 8080
+var cookieParser = require('cookie-parser') //cookie parser require
 
+//Use and Set?
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
+
 
 //this is getting my diff routes
 var urlDatabase = {
@@ -14,16 +26,11 @@ var urlDatabase = {
 
 //generates random 6 string id
 function generateRandomString() {
-  uuidv4().substr(0, 6);
+  return uuidv4().substr(0, 6);
 }
 
-//the body-parser library will allow us to access POST request parameters, such as req.body.longURL
-//the bodyParse library will allow us to acces Post REQUEST PARAMETERS
-//such as req.body.longURL which we will store in a variable called urlDatabase
-//later we will store them in a real database. for now just communication between 
-//server ad client
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+
+//addcookieparser 
 
 
 app.get("/", (req, res) => {
@@ -41,14 +48,21 @@ app.get("/hello", (req, res) => {
 // adding a route handler for /urls. using res.render()
     //to pass the URL data to your template 
 app.get("/urls",  (req, res) => {
-    let templateVars = { urls: urlDatabase };
+    let templateVars = { 
+      urls: urlDatabase,
+      username: req.cookies["username"]
+     };
     res.render("urls_index", templateVars);
 });
 
 //adding a route to urls_new.ejs
 //the order of route definitions matters
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  }
+  res.render("urls_new", templateVars);
 });
 
 //adding a route to handle shortURL requests
@@ -62,7 +76,11 @@ app.get("/u/:shortURL", (req, res) => {
 // adding a route handler for which will render it's template
 //had to add long urls, object hard to understand
 app.get("/urls/:id", (req, res) => {
-  let templateVars = {shortURL:  req.params.id, longURL: urlDatabase[req.params.id]};
+  let templateVars = {
+    shortURL:  req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"],
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -70,7 +88,7 @@ app.get("/urls/:id", (req, res) => {
 app.post("/urls", (req, res) => {
   //console.log(req.body);  // debug statement to see POST parameters
   //res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  const shortUrl = generateRandomString(); //URL part 2 assigns a random keyword 
+  let shortUrl = generateRandomString(); //URL part 2 assigns a random keyword 
   urlDatabase[shortUrl] = req.body.longURL; //URL part 2 assigning what long url we type to the random gen num
   res.redirect(`/urls/${shortUrl}`); //URL part 2 redirect to the page that has the short url 
 })
@@ -100,6 +118,21 @@ app.post("/urls/:id", (req, res) => {
   res.redirect(`/urls`);
 })
 
+//Login Post from _header
+app.post("/login", (req, res) => {
+  //res.cookie
+  let userLogin = req.body.username;
+  res.cookie("username", userLogin);//give username/whatever you input)
+  //res.redirect to urls
+  res.redirect(`/urls`);
+})
+
+//Logout Post for _header
+app.post("/logout", (req, res) => {
+  //clearcookie
+  res.clearCookie("username");
+  res.redirect(`/urls`);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
